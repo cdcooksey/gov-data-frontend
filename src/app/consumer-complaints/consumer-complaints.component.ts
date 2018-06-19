125a75135408a780585aa7e16405d0d25d66ff4b
Charlie Cooksey <cdcooksey@gmail.com>
2018-06-18 18:20:34 -0600

    clean up

--- src/app/consumer-complaints/consumer-complaints.component.ts
+++ src/app/consumer-complaints/consumer-complaints.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
 import { ConsumerComplaintService } from '../consumer-complaint.service';
 import { ConsumerComplaint } from '../consumer-complaint';
 
