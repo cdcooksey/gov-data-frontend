a9c09b5c792b47b03c996af19f074e1c52e0d4de
Charlie Cooksey <cdcooksey@gmail.com>
2018-06-18 18:12:14 -0600

    adds `consumer-complaints` component to `app.module.ts`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ConsumerComplaintsComponent } from './consumer-complaints/consumer-complaints.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsumerComplaintsComponent
   ],
   imports: [
     BrowserModule,
